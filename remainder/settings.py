9768bcfb3d2ebcc9d58321885fa89b71ba81c6bf
dennisparathanathu <dennisparathanathu@gmail.com>
2020-08-02 23:48:37 +0530

    changes made for pushing to heroku

--- remainder/settings.py
+++ remainder/settings.py
@@ -122,3 +122,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_DIR= os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS=[STATIC_DIR, ]
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
